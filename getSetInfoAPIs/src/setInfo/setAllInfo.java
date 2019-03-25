292f181c66bd21f9709e964598fcf017b9999100
serafinakamp <serafinakamp@35.1.42.103>
2019-03-24 21:53:39 -0400

    initial set up of setAllInfo

---
+++ getSetInfoAPIs/src/setInfo/setAllInfo.java
@@ -0,0 +1,40 @@
+package setInfo;
+
+import javafish.clients.opc.JOpc;
+import javafish.clients.opc.component.OpcGroup;
+import javafish.clients.opc.component.OpcItem;
+import javafish.clients.opc.exception.CoInitializeException;
+import javafish.clients.opc.exception.CoUninitializeException;
+import javafish.clients.opc.exception.ComponentNotFoundException;
+import javafish.clients.opc.exception.ConnectivityException;
+import javafish.clients.opc.exception.SynchReadException;
+import javafish.clients.opc.exception.SynchWriteException;
+import javafish.clients.opc.exception.UnableAddGroupException;
+import javafish.clients.opc.exception.UnableAddItemException;
+import javafish.clients.opc.exception.UnableRemoveGroupException;
+import javafish.clients.opc.exception.UnableRemoveItemException;
+import javafish.clients.opc.exception.UnableBrowseBranchException;
+import javafish.clients.opc.exception.UnableIBrowseException;
+import javafish.clients.opc.exception.HostException;
+import javafish.clients.opc.exception.NotFoundServersException;
+import javafish.clients.opc.exception.UnableBrowseLeafException;
+import javafish.clients.opc.variant.Variant;
+import javafish.clients.opc.variant.VariantList;
+import javafish.clients.opc.browser.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+
+public class setAllInfo {
+
+	
+	
+	
+	
+	
+	
+}
