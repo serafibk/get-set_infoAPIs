e929c6f0974d4bbefc9193d9b02cde0c8d9d9577
serafinakamp <serafinakamp@35.3.65.77>
2019-04-11 10:49:00 -0400

    add sleep time before getTag call

--- getSetInfoAPIs/src/whenDataChange/whenDataChange.java
+++ getSetInfoAPIs/src/whenDataChange/whenDataChange.java
@@ -5,13 +5,14 @@ import org.json.simple.JSONObject;
 public class whenDataChange {
 
 	
-	public String checkDataChange(String TagName, String GroupName) {
+	public String checkDataChange(String TagName, String GroupName) throws InterruptedException {
 		getAllInfo requestTag = new getAllInfo();
 		JSONObject tag = requestTag.getTag(TagName, GroupName);
 		
 		int initialVal = (int)tag.get("value");
 		int checkVal = initialVal;
 		while(checkVal == initialVal) {
+			Thread.sleep(1000);
 			JSONObject checkTag = requestTag.getTag(TagName, GroupName);
 			checkVal = (int)checkTag.get("value");
 		}
